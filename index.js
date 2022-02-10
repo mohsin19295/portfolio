25c409a0fbca7ace8d6e9f93cf967d9dc17d8ff1
mohsin19295 <mohsin19295@gmail.com>
2022-02-10 20:57:59 +0530

    changes

--- index.js
+++ index.js
@@ -93,26 +93,26 @@ window.addEventListener('scroll', () => {
 
 //GlideJs
 
-const glide = document.querySelector(".glide")
-if(glide)
-  new Glide(glide, {
-    type: 'carousel',
-    startAt: 0,
-    perView: 3,
-    gap: 30,
-    hoverpause: true,
-    autoplay: 2000,
-    animationDuration: 800,
-    animationTimingFunc: 'ease-in-out',
-    breakpoints: {
-      996:{
-        perView: 2
-      },
-      768: {
-        perView: 1
-      },
-    },
-  }).mount()
+// const glide = document.querySelector(".glide")
+// if(glide)
+//   new Glide(glide, {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 3,
+//     gap: 30,
+//     hoverpause: true,
+//     autoplay: 2000,
+//     animationDuration: 800,
+//     animationTimingFunc: 'ease-in-out',
+//     breakpoints: {
+//       996:{
+//         perView: 2
+//       },
+//       768: {
+//         perView: 1
+//       },
+//     },
+//   }).mount()
 
 new TypeIt("#type1", {
     speed: 120,
