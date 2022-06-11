e118fb66124a6c9eddd446b4fac69911927307df
mohsin19295 <mohsin19295@gmail.com>
2022-06-11 17:46:48 +0530

    about css seperated n changed

--- index.js
+++ index.js
@@ -3,68 +3,7 @@ const navOpen = document.querySelector(".hamburger")
 const navClose = document.querySelector(".close")
 const navBar = document.querySelector(".navbar")
 const navLink = document.querySelector(".nav-list")
-// const navItem = document.querySelector(".nav-link")
-const home = document.getElementById('navHome')
-const skills = document.getElementById("navSkills")
-const about = document.getElementById("navAbout")
-const project = document.getElementById("navProject")
-const github = document.getElementById("navGithub")
-const contact = document.getElementById("navContact")
 
-
-
-
-const bd = document.getElementById("bd")
-const bd2 = document.getElementById("bd2")
-
-bd.addEventListener('click', () => {
-  menu.classList.remove("show")
-})
-
-window.addEventListener('scroll', () => {
-  //home
-  if( window.pageYOffset >=58 && window.pageYOffset <571){
-    home.classList.add("highlight")
-  }else {
-    home.classList.remove("highlight")
-  }
-  //about
-  if(window.pageYOffset >=571 && window.pageYOffset <= 1280){
-    about.classList.add("highlight")
-  }else {
-    about.classList.remove("highlight")
-  }
-  //skills
-  if(window.pageYOffset >=1282 && window.pageYOffset <= 1869){
-    skills.classList.add("highlight")
-  }else {
-    skills.classList.remove("highlight")
-  }
-  //project
-  if(window.pageYOffset >=1871 && window.pageYOffset <= 2519){
-    project.classList.add("highlight")
-  }else {
-    project.classList.remove("highlight")
-  }
-
-  //github
-  if(window.pageYOffset >=2520 && window.pageYOffset <= 2800){
-    github.classList.add("highlight")
-  }else {
-    github.classList.remove("highlight")
-  }
-
-  //contact
-  if(window.pageYOffset >=2800){
-    contact.classList.add("highlight")
-  }else {
-    contact.classList.remove("highlight")
-  }
-})
-
-bd2.addEventListener('click', () => {
-  menu.classList.remove("show")
-})
 
 navOpen.addEventListener('click', () => {
   menu.classList.add("show");
@@ -82,8 +21,10 @@ navLink.addEventListener('click', () => {
 
 //navbar fixation 
 const navHeight = navBar.getBoundingClientRect().height
+// console.log('navHeight:', navHeight)
 window.addEventListener('scroll', () => {
   const scrollHeight = window.pageYOffset
+  // console.log('scrollHeight:', scrollHeight)
   if(scrollHeight > navHeight){
     navBar.classList.add('fix-nav')
   }else{
@@ -91,28 +32,6 @@ window.addEventListener('scroll', () => {
   }
 })
 
-//GlideJs
-
-// const glide = document.querySelector(".glide")
-// if(glide)
-//   new Glide(glide, {
-//     type: 'carousel',
-//     startAt: 0,
-//     perView: 3,
-//     gap: 30,
-//     hoverpause: true,
-//     autoplay: 2000,
-//     animationDuration: 800,
-//     animationTimingFunc: 'ease-in-out',
-//     breakpoints: {
-//       996:{
-//         perView: 2
-//       },
-//       768: {
-//         perView: 1
-//       },
-//     },
-//   }).mount()
 
 new TypeIt("#type1", {
     speed: 120,
