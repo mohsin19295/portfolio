2ee31628d422e354272531df09f0e0561f3f1c7f
mohsin19295 <mohsin19295@gmail.com>
2022-06-21 18:04:47 +0530

    portfolio updated

--- index.js
+++ index.js
@@ -55,3 +55,30 @@ gsap.from(".icons span", {opacity: 0, duration: 1, delay: 1.2, y: 30, stagger: 0
 
 
 
+const slider = document.querySelector('.project-flex');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 3;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+});
